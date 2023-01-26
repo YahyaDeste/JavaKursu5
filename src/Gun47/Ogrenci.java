fef39c4a9244f478e55bdd54b5366da5c84045de
TechnoStudy <ismettechno@gmail.com>
2023-01-26 18:06:19 +0000

    Initial commit

---
+++ src/Gun47/Ogrenci.java
@@ -0,0 +1,11 @@
+package Gun47;
+
+public class Ogrenci {
+    String ad;
+    String soyad;
+    int yas; //nesne yası
+
+    public void yasAta(int yas){
+        yas=yas;
+    }
+}
---
+++ src/Gun47/StudentMAin.java
@@ -0,0 +1,12 @@
+package Gun47;
+
+public class StudentMAin {
+    public static void main(String[] args) {
+
+        Ogrenci ogr=new Ogrenci();
+        ogr.yasAta(12);
+        System.out.println("ogr.yas = " + ogr.yas);
+
+
+    }
+}
